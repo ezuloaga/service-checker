1be2806019db513dd72483bcf57ecbd5506bd638
Eli <ezuloaga@users.noreply.github.com>
2022-12-29 13:05:28 -0600

    updates

--- app.py
+++ app.py
@@ -101,8 +101,7 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         flash(f'Account created for {form.username.data}!','success')
-        # return redirect(url_for('home'))
-        return redirect(url_for('summary'))
+        return redirect(url_for('home'))
     return render_template("register.html", title='Register', form=form)
 
 
@@ -112,7 +111,7 @@ def login():
     if form.validate_on_submit():
         if form.email.data == 'admin@blog.com' and form.password.data == 'password':
             flash('You have been logged in!', 'success')
-            return redirect(url_for('summary'))
+            return redirect(url_for('home'))
         else:
             flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
